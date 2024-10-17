75248986cb3be600bae220208a36b1ca878894d6
Chris <100246813+SeungwonOck@users.noreply.github.com>
2024-10-18 00:04:00 +1100

    Form Validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,37 @@
 import { Box, Button, Card, CardContent, TextField, Typography } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import Center from './Center'
+import useForm from '../hooks/useForm'
+
+const getFreshModel = () => ({
+  name: "",
+  email: "",
+})
 
 const Login = () => {
+  
+  const {
+    values,
+    setValues,
+    errors,
+    setErrors,
+    handleInputChange,
+  } = useForm(getFreshModel);
+
+  const login = (e) => {
+    e.preventDefault();
+    if(validate())
+    console.log(values)
+  }
+
+  const validate = () => {
+    let temp = {}
+    temp.email = (/\S+@\S+\.\S+/).test(values.email) ? "" : "Invalid Email"
+    temp.name = values.name != "" ? "" : "This Field is Required."
+    setErrors(temp)
+    return Object.values(temp).every(x => x==="")
+  }
+
   return (
     <Center>
       <Card sx={{ width: 400 }}>
@@ -18,16 +47,22 @@ const Login = () => {
               width: "90%"
             }
           }}>
-            <form noValidate autoComplete="off">
+            <form noValidate autoComplete="off" onSubmit={login}>
               <TextField
                 label="Email"
                 name="email"
+                value={values.email}
+                onChange={handleInputChange}
                 variant="outlined"
+                {...(errors.email && {error: true, helperText: errors.email})}
                 />
               <TextField
                 label="Name"
                 name="name"
+                value={values.name}
+                onChange={handleInputChange}
                 variant="outlined"
+                {...(errors.name && {error: true, helperText: errors.name})}
                 />
               <Button
                 type="submit"
